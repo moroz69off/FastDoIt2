0a681c1fd6cbdfff70674b7b9ea6bf9bcfdaef70
Морозов Юрий <moroz69off@gmail.com>
2020-11-04 11:40:14 +0300

    Add resource

--- FastDoIt/FastDoIt/Program.cs
+++ FastDoIt/FastDoIt/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,31 @@ namespace FastDoIt
     {
         static void Main(string[] args)
         {
+            using (ResourceWriter RW = new ResourceWriter(@".\CarResources.resources"))
+            {
+                RW.AddResource("isActionGoodResult", false);
+            }
+            Actor actor = new Actor(args[0]);
+                Console.ReadLine();
+        }
+        class Actor
+        {
+            public Actor()
+            {
+                if (true)
+                {
+                    isGoodResult = true;
+                }
+            }
+
+            public Actor(string result)
+            {
+                if (result=="o_0")
+                {
+                    isGoodResult = true;
+                }
+            }
+            bool isGoodResult { get; set; } = false;
         }
     }
 }
