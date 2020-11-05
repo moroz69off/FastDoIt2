6fe53d14a2577e4a891167f96a3bb0236a16ec56
moroz69off <moroz69off@gmail.com>
2020-11-05 23:15:06 +0300

    Get driver

--- FastDoIt/FastDoIt/Program.cs
+++ FastDoIt/FastDoIt/Program.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace FastDoIt
 {
@@ -15,9 +17,31 @@ namespace FastDoIt
             {// add resources here
                 RW.AddResource("isActionGoodResult", false);
             }
+
+            Console.Title = "Fast DO IT";
+
+            GetDriver();
+
             Actor actor = new Actor("o_0");
                 Console.ReadLine();
         }
+
+        private static void GetDriver()
+        {
+            try
+            {
+                string command = "setx / m path \"%path%;C:\\WebDriver\\bin\\\"";
+                var v = Console.OpenStandardInput();
+                var dir = Directory.GetCurrentDirectory();
+                var files = Directory.GetFiles(dir, "webdriver.exe");
+                if (files[0] != "webdriver.exe") Console.WriteLine($"Current directory ({dir}) should contains file \"webdriver.exe\"");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"File \"webdriver.exe\" not found\n * * * \nSystem error message:\n\t{ex.Message}", "File driwer error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
         class Actor
         {
             public Actor()
