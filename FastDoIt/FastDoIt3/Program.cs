b1f2d79b8e7eaaad7925cadfea6563fed91b65a6
moroz69off <moroz69off@gmail.com>
2020-11-10 08:36:17 +0300

    driver ok

--- FastDoIt/FastDoIt3/Program.cs
+++ FastDoIt/FastDoIt3/Program.cs
@@ -5,13 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace FastDoIt3
 {
     class Program
     {
+        public static string sessionId { get; private set; }
+
         static void Main(string[] args)
         {
+            IWebDriver driver = new ChromeDriver();
+
+            ChromeOptions options = new ChromeOptions();
+            options.SetLoggingPreference("Browser", LogLevel.Off);
+            options.SetLoggingPreference("Driver" , LogLevel.Off);
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://kith.com");
+            sessionId = driver.CurrentWindowHandle;
+            Console.WriteLine(sessionId);
+            Console.ReadLine();
         }
     }
 }
