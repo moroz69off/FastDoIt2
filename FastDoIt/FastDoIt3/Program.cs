522ceb3c8aee9cd229be624c8b123e86a80fef82
moroz69off <moroz69off@gmail.com>
2020-11-11 01:46:03 +0300

    I want to sleep

--- FastDoIt/FastDoIt3/Program.cs
+++ FastDoIt/FastDoIt3/Program.cs
@@ -15,29 +15,44 @@ namespace FastDoIt3
         public static int processId { get; private set; }
 
         //backToShop glDefaultBtn
+        //https://kith.com/collections/stone-island-ghost-capsule/products/simo7315s02f6-v0070
         //backToShop glDefaultBtn
         static void Main(string[] args)
+        {
+            ChromeDriverService chromeDriverService = AddService();
+
+            ChromeOptions options = new ChromeOptions();
+            options.SetLoggingPreference("Browser", LogLevel.All);
+            options.SetLoggingPreference("Driver", LogLevel.All);
+            options.AddArguments("--disable-infobars");
+            
+            object preference = null;
+            options.AddLocalStatePreference("", preference);
+
+            IWebDriver driver = new ChromeDriver(chromeDriverService, options);
+            _ = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            sessionId = driver.CurrentWindowHandle;
+            Console.WriteLine($"sessionId: {sessionId}");
+
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://kith.com/collections/stone-island-ghost-capsule/products/simo7315s02f6-v0070");
+
+
+
+            driver.Quit();
+            Console.ReadLine();
+        }
+
+        private static ChromeDriverService AddService()
         {
             ChromeDriverService chromeDriverService = ChromeDriverService.CreateDefaultService();
             chromeDriverService.HideCommandPromptWindow = true;
             chromeDriverService.SuppressInitialDiagnosticInformation = true;
             processId = chromeDriverService.ProcessId;
             Console.WriteLine($"Process Id: {processId}");
-
-            ChromeOptions options = new ChromeOptions();
-            options.SetLoggingPreference("Browser", LogLevel.All);
-            options.SetLoggingPreference("Driver" , LogLevel.All);
-            options.AddArgument("--disable-infobars");
-
-            IWebDriver driver = new ChromeDriver(chromeDriverService, options);
-            _ = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-
-            driver.Manage().Window.Maximize();
-
-            driver.Navigate().GoToUrl("https://yumor.xyz");
-            sessionId = driver.CurrentWindowHandle;
-            Console.WriteLine($"sessionId: {sessionId}");
-            Console.ReadLine();
+            return chromeDriverService;
         }
     }
 }
