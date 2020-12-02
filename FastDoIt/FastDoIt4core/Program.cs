784c73890bf699c23859bb25f91377ded49ca4b5
moroz69off <moroz69off@gmail.com>
2020-12-02 23:00:52 +0300

    args fail

--- FastDoIt/FastDoIt4core/Program.cs
+++ FastDoIt/FastDoIt4core/Program.cs
@@ -13,14 +13,42 @@ namespace FastDoIt4core
 
         public static bool isDebug { get; private set; } = false;
 
-        public static string switchOn { get; private set; } = "";
-
         public static List<string> ProfileInfoList { get; private set; }
 
         static int timeout = 3500, interval = 777; // default
 
         static void Main(string[] args)
         {
+            Console.Title = "FastDoIt";
+            if (args.Length > 0)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    switch (args[i])
+                    {
+                        case "-D":
+                            Console.WriteLine("Debug mode enabled.");
+                            isDebug = true;
+                            break;
+                        case "-T":
+                            Console.WriteLine($"Timeout = {int.Parse(args[i + 1])}");
+                            timeout = int.Parse(args[++i]);
+                            break;
+                        case "-I":
+                            Console.WriteLine($"Interval = {int.Parse(args[i + 1])}");
+                            interval = int.Parse(args[i + 1]);
+                            break;
+                        case "-P":
+                            Console.WriteLine($"Profile info: \"{args[i + 1]}\"");
+                            ProfileInfoList = new List<string>(args[i + 1].Trim(new char['"']).Split(new char[',']));
+                            break;
+                        default:
+                            Console.WriteLine($"Parameter №{i} is not recognized");
+                            break;
+                    }
+                }
+            } // get args values
+
 
             Console.ReadLine();
         }
