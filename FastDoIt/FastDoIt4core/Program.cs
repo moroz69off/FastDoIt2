31a63826579d20554d012c37c4afe29cfc165aca
moroz69off <moroz69off@gmail.com>
2020-12-03 21:50:49 +0300

    zavelas

--- FastDoIt/FastDoIt4core/Program.cs
+++ FastDoIt/FastDoIt4core/Program.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
 
 namespace FastDoIt4core
 {
@@ -16,51 +19,82 @@ namespace FastDoIt4core
 
         public static List<string> ProfileInfoList { get; private set; }
 
+        public static Func<string> FasFunc { get; set; }
+
+        static IWebDriver webDriver;
+
         static int timeout = 3500, interval = 777; // default
 
-        //static void Main(string[] args)
-        //{
-        //    Console.Title = "FastDoIt";
-        //    if (args.Length > 0)
-        //    {
-        //        for (int i = 0; i < args.Length; i++)
-        //        {
-        //            switch (args[i])
-        //            {
-        //                case "-D":
-        //                    Console.WriteLine("Debug mode enabled.");
-        //                    isDebug = true;
-        //                    break;
-        //                case "-T":
-        //                    Console.WriteLine($"Timeout = {int.Parse(args[i + 1])}");
-        //                    timeout = int.Parse(args[++i]);
-        //                    break;
-        //                case "-I":
-        //                    Console.WriteLine($"Interval = {int.Parse(args[i + 1])}");
-        //                    interval = int.Parse(args[i + 1]);
-        //                    break;
-        //                case "-P":
-        //                    Console.WriteLine($"Profile info: \"{args[i + 1]}\"");
-        //                    ProfileInfoList = new List<string>(args[i + 1].Trim(new char['"']).Split(new char[',']));
-        //                    break;
-        //                default:
-        //                    Console.WriteLine($"Parameter №{i} is not recognized");
-        //                    break;
-        //            }
-        //        }
-        //    } // get args values
+        private static string str;
 
+        private static int profileNum;
 
-        //}
-        static async System.Threading.Tasks.Task Main(string[] args)
+        static void Main(string[] args)
         {
-            int counter = 0;
-            int max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
-            while (max == -1 || counter < max)
+            Console.Title = "FastDoIt";
+            if (args.Length > 0)
             {
-                Console.WriteLine($"Counter: {++counter}");
-                await System.Threading.Tasks.Task.Delay(777);
-            }
+                for (int i = 0; i < args.Length; i++)
+                {
+                    switch (args[i])
+                    {
+                        case "-D":
+                            Console.WriteLine("Debug mode enabled.");
+                            isDebug = true;
+                            break;
+                        case "-T":
+                            Console.WriteLine($"Timeout = {int.Parse(args[i + 1])}");
+                            timeout = int.Parse(args[++i]);
+                            break;
+                        case "-I":
+                            Console.WriteLine($"Interval = {int.Parse(args[i + 1])}");
+                            interval = int.Parse(args[i + 1]);
+                            break;
+                        case "-P":
+                            Console.WriteLine($"Profile info: \"{args[i + 1]}\"");
+                            string[] profiles = System.IO.File.ReadAllLines("profiles.csv");
+                            profileNum = int.Parse(args[i + 1]);
+                            ProfileInfoList = new List<string>(profiles[profileNum + 1].Trim(new char['"']).Split(new char[',']));
+                            break;
+                        default:
+                            Console.WriteLine($"Parameter №{i} is not recognized");
+                            break;
+                    }
+                }
+            } // get args values
+
+            if (!isDebug)
+                ProfileInfoList = GetProfile("profiles.csv", profileNum); // 1 - fisrt profile in profiles list (for multiprofiles work)
+
+            webDriver = new ChromeDriver();
+
+            _ = FastDoItTask();
+        }
+
+        private static List<string> GetProfile(string path, int num)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async System.Threading.Tasks.Task FastDoItTask()
+        {
+            object ob = null;
+            AsyncCallback call = null;
+            FasFunc.BeginInvoke(call, ob);
+            str = await WriteLog("fast.log", "driver stared " +
+                DateTime.Now.ToString() +
+                " | driver clock " +
+                clock.Now +
+                " | process " +
+                webDriver.WindowHandles + webDriver.Manage().Logs
+                );
+        }
+
+        private static Task<string> WriteLog(string path, string message)
+        {
+            Console.WriteLine(message);
+            System.IO.File.AppendAllTextAsync(path, message);
+            return new Task<string>(FasFunc);
         }
     }
 }
---
+++ FastDoIt/FastDoIt4core/docs/FastDoIt4coreU.md
@@ -0,0 +1,13 @@
+# FastDoIt!
+______________________
+This web application requires some dependencies:
+
+*for windows -*
+- Selenium web driver (driver for chrome, dounload automatic)
+- Web brouser Chrome 
+- .NET Core 3.1 [Dounload](https://dotnet.microsoft.com/download/dotnet-core/3.1)
+
+*for linux -*
+- Web brouser Chrome
+- .NET CORE 3.1
+- 
