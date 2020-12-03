dc74de31c966f6ada51fa16f7a3770bd214344e4
moroz69off <moroz69off@gmail.com>
2020-12-03 19:00:58 +0300

    added docs new ver

--- FastDoIt/FastDoIt4core/Program.cs
+++ FastDoIt/FastDoIt4core/Program.cs
@@ -1,13 +1,14 @@
 ﻿using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace FastDoIt4core
 {
     class Program
     {
-        private static readonly string checkoutPath = "https://kith.com"+
-            "/pages/international-checkout#Global-e_International_Checkout";
+        private static readonly string checkoutPath = "ht"+"tp"+"s:"+"//"+"ki"+"th"+".c"+"om"+
+            "/pages/international"+"-"+"checkout#Global-e_International_Checkout";
 
         private static readonly IClock clock = new SystemClock();
 
@@ -17,39 +18,49 @@ namespace FastDoIt4core
 
         static int timeout = 3500, interval = 777; // default
 
-        static void Main(string[] args)
+        //static void Main(string[] args)
+        //{
+        //    Console.Title = "FastDoIt";
+        //    if (args.Length > 0)
+        //    {
+        //        for (int i = 0; i < args.Length; i++)
+        //        {
+        //            switch (args[i])
+        //            {
+        //                case "-D":
+        //                    Console.WriteLine("Debug mode enabled.");
+        //                    isDebug = true;
+        //                    break;
+        //                case "-T":
+        //                    Console.WriteLine($"Timeout = {int.Parse(args[i + 1])}");
+        //                    timeout = int.Parse(args[++i]);
+        //                    break;
+        //                case "-I":
+        //                    Console.WriteLine($"Interval = {int.Parse(args[i + 1])}");
+        //                    interval = int.Parse(args[i + 1]);
+        //                    break;
+        //                case "-P":
+        //                    Console.WriteLine($"Profile info: \"{args[i + 1]}\"");
+        //                    ProfileInfoList = new List<string>(args[i + 1].Trim(new char['"']).Split(new char[',']));
+        //                    break;
+        //                default:
+        //                    Console.WriteLine($"Parameter №{i} is not recognized");
+        //                    break;
+        //            }
+        //        }
+        //    } // get args values
+
+
+        //}
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
-            Console.Title = "FastDoIt";
-            if (args.Length > 0)
+            int counter = 0;
+            int max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+            while (max == -1 || counter < max)
             {
-                for (int i = 0; i < args.Length; i++)
-                {
-                    switch (args[i])
-                    {
-                        case "-D":
-                            Console.WriteLine("Debug mode enabled.");
-                            isDebug = true;
-                            break;
-                        case "-T":
-                            Console.WriteLine($"Timeout = {int.Parse(args[i + 1])}");
-                            timeout = int.Parse(args[++i]);
-                            break;
-                        case "-I":
-                            Console.WriteLine($"Interval = {int.Parse(args[i + 1])}");
-                            interval = int.Parse(args[i + 1]);
-                            break;
-                        case "-P":
-                            Console.WriteLine($"Profile info: \"{args[i + 1]}\"");
-                            ProfileInfoList = new List<string>(args[i + 1].Trim(new char['"']).Split(new char[',']));
-                            break;
-                        default:
-                            Console.WriteLine($"Parameter №{i} is not recognized");
-                            break;
-                    }
-                }
-            } // get args values
-
-
+                Console.WriteLine($"Counter: {++counter}");
+                await System.Threading.Tasks.Task.Delay(777);
+            }
         }
     }
 }
